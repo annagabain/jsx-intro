075bd983e0882c74916b59b902a3b0d46e406bef
Anna Gabain <anna.gabain@outlook.com>
2023-03-10 13:54:35 +0000

    Try App.js without JSX

--- jsx-example/src/App.js
+++ jsx-example/src/App.js
@@ -3,26 +3,26 @@ import './App.css';
 
 function App() {
   // With JSX:
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <h1>Hello from JSX!</h1>
-  //     </header>
-  //   </div>
-  // );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello from JSX!</h1>
+      </header>
+    </div>
+  );
 
   // Without JSX:
-  return React.createElement(
-    "div",
-    { className: "App" },
-    React.createElement("header",
-      { className: "App-header" },
-      React.createElement("h1",
-        null,
-        "Hello from JSX!"
-      )
-    )
-  );
+  // return React.createElement(
+  //   "div",
+  //   { className: "App" },
+  //   React.createElement("header",
+  //     { className: "App-header" },
+  //     React.createElement("h1",
+  //       null,
+  //       "Hello from JSX without JSX!"
+  //     )
+  //   )
+  // );
 }
 
 export default App;
